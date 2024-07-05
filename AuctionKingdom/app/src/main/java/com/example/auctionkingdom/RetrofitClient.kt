35a1dc73e1eca9ee8f81cc0ba5e38fee0bfdc575
choseongje <hangoy0606@gmail.com>
2024-07-05 21:39:00 +0900

    server 연결

--- AuctionKingdom/app/src/main/java/com/example/auctionkingdom/RetrofitClient.kt
+++ AuctionKingdom/app/src/main/java/com/example/auctionkingdom/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:5001"
+    private const val BASE_URL = "http://172.10.7.80"
 
     private val logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
     private val client = OkHttpClient.Builder().addInterceptor(logger).build()
