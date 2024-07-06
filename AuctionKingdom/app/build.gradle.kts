1ebfd726560901af1e4b61f4778655f565a27fb8
choseongje <hangoy0606@gmail.com>
2024-07-06 16:59:31 +0900

    메인화면 CardView 설정

--- AuctionKingdom/app/build.gradle.kts
+++ AuctionKingdom/app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.converter.gson)
     implementation(libs.logging.interceptor)
     implementation(libs.okhttp)
+    implementation(libs.androidx.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
